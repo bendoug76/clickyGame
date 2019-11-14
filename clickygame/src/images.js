ffda12d96b01454e8abdfc3a15346e4a80bd02f8
Benjamin Lee <benjamindlee4@gmail.com>
2019-11-13 22:12:18 -0600

    added skeleton and images

---
+++ clickygame/src/images.js
@@ -0,0 +1,35 @@
+import badGuy from './images/badGuy.png';
+import cardinal from './images/cardinal.png';
+import charles from './images/charles.png';
+import crozier from './images/crozier.png';
+import dick from './images/dick.png';
+import drRockzo from './images/drRockzo.png';
+import host from './images/host.png';
+import jean from './images/jean.png';
+import klokateer from './images/klokateer.png';
+import maskedAssassin from './images/maskedAssassin.png';
+import murderface from './images/murderface.png';
+import nathan from './images/nathan.png';
+import pickles from './images/pickles.png';
+import skwizgaar from './images/skwizgaar.png';
+import toki from './images/toki.png';
+
+const images = [
+    badGuy,
+    cardinal ,
+    charles,
+    crozier, 
+    dick,
+    drRockzo,
+    host,
+    jean,
+    klokateer,
+    maskedAssassin,
+    murderface, 
+    nathan,
+    pickles,
+    skwizgaar, 
+    toki 
+]
+
+export default images;
